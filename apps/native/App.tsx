95196e53e3a858d8cfd89684acb5676224dd13bd
t-skgm <skgm0301@gmail.com>
2024-09-21 13:03:57 +0900

    fix: native with esm

---
+++ apps/native/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// import { Button } from "@repo/ui/button";
+import { Code } from "@repo/ui/code";
+console.log(Code);
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      {/* <Button appName="native">Press!</Button> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
